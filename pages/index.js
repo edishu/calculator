807c2d53f74d8481951751f51d0ce9fa45083e8f
Udayan Maurya <edmauryaishu@gmail.com>
2021-06-25 15:33:11 -0400

    Created keypad layout

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
       <div className="font-spartan w-1/3">
-        <section className="flex justify-between w-full text-topText1">
-          <h2 className="">calc</h2>
+        <section className="flex justify-between items-end w-full text-topText1 mb-8">
+          <h2 className="text-3xl">calc</h2>
           <div className="flex items-end">
             <h3 className="mr-5 text-sm">THEME</h3>
             <div className="grid grid-cols-3 grid-rows-2 gap-x-4 gap-y-1">
@@ -35,10 +35,49 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="flex justify-between w-full">
-          <h2>399,881</h2>
+        <section className="flex justify-end w-full bg-screenBg1 text-topText1 p-8 rounded-lg mb-8">
+          <h2 className="text-4xl">399,881</h2>
+        </section>
+        <section className="bg-keypadBg1 p-8 rounded-lg">
+          <div className="grid grid-cols-4 grid-rows-5 gap-4">
+            <Key>7</Key>
+            <Key>8</Key>
+            <Key>9</Key>
+            <p className="rounded-lg border-b-4 flex flex-col justify-center pt-2 pb-1 bg-delBg1 border-delShad1 text-delText1 text-center">
+              DEL
+            </p>
+            <Key>4</Key>
+            <Key>5</Key>
+            <Key>6</Key>
+            <Key>+</Key>
+            <Key>1</Key>
+            <Key>2</Key>
+            <Key>3</Key>
+            <Key>-</Key>
+            <Key>.</Key>
+            <Key>0</Key>
+            <Key>/</Key>
+            <Key>x</Key>
+
+            <p className="rounded-lg border-b-4 flex flex-col justify-center pt-2 pb-1 bg-delBg1  border-delShad1 text-delText1 text-center col-span-2">
+              RESET
+            </p>
+            <p className="rounded-lg border-b-4 flex flex-col justify-center pt-2 pb-1 bg-eqBg1  border-eqShad1 text-delText1 text-center col-span-2">
+              =
+            </p>
+          </div>
         </section>
       </div>
     </div>
   );
 }
+
+const Key = ({ children, className }) => {
+  return (
+    <p
+      className={`rounded-lg border-b-4 flex flex-col justify-center pt-2 pb-1 bg-keyBg1 border-keyShad1 text-keyText1 text-center text-3xl ${className}`}
+    >
+      {children}
+    </p>
+  );
+};
